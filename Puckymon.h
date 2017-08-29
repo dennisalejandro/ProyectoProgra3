4a869dffd697e8dafe4e257cddc927079a803456
dennisalejandro <dennisalejandro@unitec.edu>
2017-08-29 11:36:17 -0600

    Add files via upload

---
+++ Puckymon.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class {
+	private:
+		int HP;
+		int ATK;
+		int SpATK;
+		int DEF;
+		int SpDef;
+	public:
+};
---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace std;
+int main() {
+	Application::Run(gcnew ProyectoProgra::MyForm);
+	return 0;
+}
